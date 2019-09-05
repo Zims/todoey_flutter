48fa98b3791e7ad967821b55ced4325f8b18843c
Zims <zims@inbox.lv>
2019-09-05 10:29:30 +0300

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todoey_flutter/screens/task_screen.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
